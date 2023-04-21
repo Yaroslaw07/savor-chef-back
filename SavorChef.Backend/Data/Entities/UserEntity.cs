814c482785b1bfbb81fb13e98435e7b53b4bb4fb
Ihor <flisfish@ukr.net>
2023-04-21 19:13:02 +0200

    Add UserEntity

--- SavorChef.Backend/Data/Entities/UserEntity.cs
+++ SavorChef.Backend/Data/Entities/UserEntity.cs
@@ -2,6 +2,7 @@
 
     public class UserEntity
     {
+        public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
 
         public string Password { get; set; } = string.Empty;
